dc388c5f877577fd46abf337444575fe9818dfa5
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-23 12:24:05 -0300

    build(maven): inicializa projeto e adiciona dependências necessárias
    Adicionado o arquivo pom.xml com as dependências iniciais para o projeto Maven.
    Inclui configurações básicas para compilação e gerenciamento de pacotes.
    
    Closes #1

---
+++ backend/sellers-backend/src/main/java/com/sellers/sellers_backend/SellersBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sellers.sellers_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellersBackendApplication.class, args);
+	}
+
+}
---
+++ backend/sellers-backend/src/test/java/com/sellers/sellers_backend/SellersBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sellers.sellers_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellersBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
