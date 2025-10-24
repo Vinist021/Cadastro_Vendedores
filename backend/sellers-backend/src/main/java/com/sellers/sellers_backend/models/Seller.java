d29aeaf9c33f77df50dc7fc1ee76eaaa1ba727e4
Vinicius Teixeira <vinist.002@gmail.com>
2025-10-24 12:59:08 -0300

    feat(models): retira constraints da entidade Seller devido à existencia de DTOs

--- backend/sellers-backend/src/main/java/com/sellers/sellers_backend/models/Seller.java
+++ backend/sellers-backend/src/main/java/com/sellers/sellers_backend/models/Seller.java
@@ -2,17 +2,11 @@ package com.sellers.sellers_backend.models;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
-import org.hibernate.validator.constraints.Length;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.DecimalMax;
-import jakarta.validation.constraints.DecimalMin;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
 
 @Entity
 @Table(name = "sellers")
@@ -21,22 +15,16 @@ public class Seller {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @Column(name = "name", nullable = false)
-    @Length(min = 5, max=255, message = "Name length must be between 5 and 255 characters")
+    @Column(name = "name", length = 255, nullable = false)
     private String name;
 
     @Column(name = "salary", nullable = false)
-    @DecimalMin(value = "0.0", message = "Salary must be at least 0")
     private Double salary;
 
     @Column(name = "bonus", nullable = false)
-    @DecimalMin(value = "0.0", message = "Bonus must be between 0 and 100")
-    @DecimalMax(value = "100.0", message = "Bonus must be between 0 and 100")
     private Double bonus;
 
     @Column(name = "gender", nullable = false)
-    @Min(value = 0, message = "Gender must be 0 (Female) or 1 (Male)")
-    @Max(value = 1, message = "Gender must be 0 (Female) or 1 (Male)")
     private Integer gender;
 
     public Seller() {
